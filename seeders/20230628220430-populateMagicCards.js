d5a64c2a32bbd381d2546745cbe1e3b52857cb5e
Wilson Santos <pw.druida@gmail.com>
2023-06-28 19:20:21 -0300

    populate tables

---
+++ seeders/20230628220430-populateMagicCards.js
@@ -0,0 +1,144 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const cards = [
+      {
+        name: 'Exemplar of Strength',
+        manaCost: '{1}{G}',
+        type: 'Creature - Human Warrior',
+        rarity: 'Common',
+        color: 'Green',
+        collection: 'AFR',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+        {
+          name: 'Dungeon Crawler',
+          manaCost: '{1}{B}',
+          type: 'Creature - Goblin Rogue',
+          rarity: 'Common',
+          color: 'Black',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Bard Class',
+          manaCost: '{1}{U}',
+          type: 'Enchantment - Class',
+          rarity: 'Uncommon',
+          color: 'Blue',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Cave of the Frost Dragon',
+          manaCost: '',
+          type: 'Land',
+          rarity: 'Rare',
+          color: 'Colorless',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Minimus Containment',
+          manaCost: '{1}{W}',
+          type: 'Enchantment',
+          rarity: 'Uncommon',
+          color: 'White',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'You Find a Cursed Idol',
+          manaCost: '{2}{R}',
+          type: 'Enchantment',
+          rarity: 'Rare',
+          color: 'Red',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Fly',
+          manaCost: '{G}',
+          type: 'Sorcery - Adventure',
+          rarity: 'Common',
+          color: 'Green',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Delina, Wild Mage',
+          manaCost: '{2}{R}{R}',
+          type: 'Legendary Creature - Elf Shaman',
+          rarity: 'Rare',
+          color: 'Red',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'You See a Pair of Goblins',
+          manaCost: '{1}{R}',
+          type: 'Sorcery - Adventure',
+          rarity: 'Common',
+          color: 'Red',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Portable Hole',
+          manaCost: '{W}',
+          type: 'Artifact',
+          rarity: 'Uncommon',
+          color: 'Colorless',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Wandering Troubadour',
+          manaCost: '{1}{W}',
+          type: 'Creature - Human Bard',
+          rarity: 'Common',
+          color: 'White',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Book of Vile Darkness',
+          manaCost: '{B}{B}{B}',
+          type: 'Legendary Artifact',
+          rarity: 'Mythic',
+          color: 'Colorless',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Gloom Stalker',
+          manaCost: '{1}{B}',
+          type: 'Creature - Human Ranger',
+          rarity: 'Uncommon',
+          color: 'Black',
+          collection: 'AFR',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+    ];
+
+    await queryInterface.bulkInsert('MagicCards', cards, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('MagicCards', null, {});
+  }
+};
